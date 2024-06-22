b2fb74087f6060a711414ac365f3b3c2d6904929
Ryan Nolan <ryannolan_@hotmail.com>
2024-06-22 10:19:21 +0000

    Add custom user model

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Custom user model extending the built-in AbstractUser
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('client', 'Client'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.username
