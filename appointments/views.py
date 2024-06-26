f04cfd202df10d79292f6597c795543bc4bd2a9e
Ryan Nolan <ryannolan_@hotmail.com>
2024-06-26 18:49:29 +0000

    Update view filter

--- appointments/views.py
+++ appointments/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.utils import timezone
 from .models import Appointment
 
 # Create your views here.
 class AppointmentList(generic.ListView):
-    queryset = Appointment.objects.all()
-    template_name = "appointments/appointment_list.html"
+    queryset = Appointment.objects.filter(date__gte=timezone.now()).order_by("date")
+    template_name = "appointment_list.html"
