98cc3bee9fc86fb0bb32252e451866b1cccccfa2
Ryan Nolan <ryannolan_@hotmail.com>
2024-06-26 19:14:05 +0000

    Update the appointment app to use summernote

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import User, Appointment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(SummernoteModelAdmin):
+    list_display = ('client', 'date', 'notes')
+    search_fields = ['client__username']
+    list_filter = ('date',)
+    summernote_fields = ('notes',)
+
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Appointment)
+
