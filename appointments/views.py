9e7b3f95dceddaebea353e93c007f260b932d885
Ryan Nolan <ryannolan_@hotmail.com>
2024-06-26 22:12:00 +0000

    Add base and index templates

--- appointments/views.py
+++ appointments/views.py
@@ -6,4 +6,6 @@ from .models import Appointment
 # Create your views here.
 class AppointmentList(generic.ListView):
     queryset = Appointment.objects.filter(date__gte=timezone.now()).order_by("date")
-    template_name = "appointment_list.html"
+    template_name = "appointments/index.html"
+    paginate_by = 6
+
