e51cebde56d52f9eb4da1307b2e575e531b3d656
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-01 14:55:27 +0000

    Register model in admin

--- about/admin.py
+++ about/admin.py
@@ -6,3 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
+@admin.register(ContactRequest)
+class ContactRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'read', 'created_on')
+    list_filter = ('read', 'created_on')
+    search_fields = ('name', 'email')
+
