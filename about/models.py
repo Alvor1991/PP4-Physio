cf8e92246764594c2d15d2601e5fc880b8d0f8d6
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-01 14:54:37 +0000

    Update models

--- about/models.py
+++ about/models.py
@@ -8,3 +8,13 @@ class About(models.Model):
     def __str__(self):
         return self.title
 
+class ContactRequest(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
