9b1cb803440c6a5f153aec9258e8bbe00779c294
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-01 15:01:38 +0000

    Modify about views to handle form

--- about/views.py
+++ about/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
-from .models import About
+from django.shortcuts import render, redirect
+from .models import About, ContactRequest
+from .forms import ContactForm
+from django.contrib import messages
 
 def about_me(request):
     """
-    Renders the About page
+    Renders the About page and handles the contact form submission
     """
     about = About.objects.all().order_by('-updated_on').first()
+    contact_form = ContactForm()
+
+    if request.method == "POST":
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your message has been sent successfully.'
+            )
+            return redirect('about')
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "contact_form": contact_form
+        },
     )
+
 
