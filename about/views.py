8edb731b4b4c3e20d30b82ca3a6503d8e917dbb0
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-01 15:17:28 +0000

    Update about views to handle post

--- about/views.py
+++ about/views.py
@@ -1,32 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib import messages
 from .models import About, ContactRequest
 from .forms import ContactForm
-from django.contrib import messages
 
 def about_me(request):
-    """
-    Renders the About page and handles the contact form submission
-    """
-    about = About.objects.all().order_by('-updated_on').first()
-    contact_form = ContactForm()
-
     if request.method == "POST":
-        contact_form = ContactForm(request.POST)
+        contact_form = ContactForm(data=request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Your message has been sent successfully.'
-            )
-            return redirect('about')
+            messages.add_message(request, messages.SUCCESS, "Your message has been sent successfully. I endeavor to respond within 2 working days.")
+
+    about = About.objects.all().order_by('-updated_on').first()
+    contact_form = ContactForm()
 
     return render(
         request,
         "about/about.html",
         {
             "about": about,
-            "contact_form": contact_form
+            "contact_form": contact_form,
         },
     )
-
-
