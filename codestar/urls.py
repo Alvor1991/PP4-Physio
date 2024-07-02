5d1b53cc11834f5282867bdbb5c83c09c1636d6f
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-02 14:47:49 +0000

    Update home and appointment URLs

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,12 +19,13 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html"), name='home'), 
+    path('', TemplateView.as_view(template_name="home/home.html"), name='home'), 
     path('about/', include('about.urls'), name='about-urls'),
     path('treatments/', include('treatments.urls'), name='treatments-urls'),
     path('appointments/', include('appointments.urls'), name='appointments'),
     path('admin/', admin.site.urls),
 ]
+
 
 
 
