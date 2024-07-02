187abe7ac36c06f62f517f915a6cc0efd152affd
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-02 12:31:52 +0000

    Remove appointment detail from urls and delete the file

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.AppointmentList.as_view(), name='appointments'),
-    path('<int:id>/', views.appointment_detail, name='appointment_detail'), 
     path('book/', views.book_appointment, name='book_appointment'),
 ]
-
-
