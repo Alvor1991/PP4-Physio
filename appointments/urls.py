79b29817afa34fed57497f2dabe2bbc47a17f220
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-02 11:52:51 +0000

    Update URLs to point to the new booking view

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.AppointmentList.as_view(), name='appointments'),
+    path('<int:id>/', views.appointment_detail, name='appointment_detail'), 
     path('book/', views.book_appointment, name='book_appointment'),
-    path('<int:id>/', views.appointment_detail, name='appointment_detail'),
 ]
 
 
