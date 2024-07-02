1ba52c72ff2ddb180e71213b75b7e01c41c78d77
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-02 11:47:04 +0000

    Capture appointment details without requiring login

--- appointments/forms.py
+++ appointments/forms.py
@@ -4,4 +4,7 @@ from .models import Appointment
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
-        fields = ['date', 'notes']
+        fields = ['date', 'notes', 'client_name', 'client_email']
+        widgets = {
+            'date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
