3d7d99937cb63fc1f07f708d87fa6dfba02e197b
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-02 12:38:08 +0000

    Remove index file and appointments list view from URLs

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,6 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.AppointmentList.as_view(), name='appointments'),
     path('book/', views.book_appointment, name='book_appointment'),
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .forms import AppointmentForm
 
+class AppointmentList(generic.ListView):
+    queryset = Appointment.objects.all().order_by("date")
+    template_name = "appointments/index.html"
+    paginate_by = 6
+
 def book_appointment(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
