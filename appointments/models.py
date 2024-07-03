fc3eb6568747f3fe4c7f771e668ca1405cc5c10c
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-03 13:45:53 +0000

    Add time field to models

--- appointments/models.py
+++ appointments/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Appointment model
 class Appointment(models.Model):
     client_name = models.CharField(max_length=100, default='default_name')
     client_email = models.EmailField(default='default@example.com')
-    date = models.DateTimeField()
+    date = models.DateField()
+    time = models.TimeField()
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"Appointment for {self.client_name} on {self.date}"
-
+        return f"Appointment for {self.client_name} on {self.date} at {self.time}"
