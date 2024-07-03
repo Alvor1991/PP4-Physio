875f9d41c269538a989411e9f3865d29fefd6a07
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-03 13:52:52 +0000

    Update URLs

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('book/', views.book_appointment, name='book_appointment'),
+    path('get_available_time_slots/', views.available_time_slots_view, name='get_available_time_slots'),
 ]
--- codestar/urls.py
+++ codestar/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -25,10 +26,4 @@ urlpatterns = [
     path('appointments/', include('appointments.urls'), name='appointments'),
     path('admin/', admin.site.urls),
 ]
-
-
-
-
-
-
 
