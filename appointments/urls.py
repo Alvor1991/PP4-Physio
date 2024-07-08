cd8157c0c4436068434b3d2f990e630109b4c4a5
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-08 19:44:03 +0000

    Add a new URL pattern for the

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('book/', views.book_appointment, name='book_appointment'),
+    path('get_time_slots/', views.get_time_slots, name='get_time_slots'),
 ]
