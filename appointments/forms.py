cca8635dad9143be8e7d1271ca709102ab0f4985
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-08 19:42:01 +0000

    Add date input class, time slot field and modify init method

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,15 +1,29 @@
 from django import forms
 from .models import Appointment
+from .utils import get_available_time_slots
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class AppointmentForm(forms.ModelForm):
+    date = forms.DateField(widget=DateInput)
+    time_slot = forms.ChoiceField(choices=[], required=True, label="Time Slot")
+
     class Meta:
         model = Appointment
-        fields = ['date', 'time', 'notes', 'client_name', 'client_email']
-        widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
-            'time': forms.TimeInput(attrs={'type': 'time', 'step': 300}),
-        }
+        fields = ['client_name', 'client_email', 'date', 'time_slot', 'notes']
 
+    def __init__(self, *args, **kwargs):
+        super(AppointmentForm, self).__init__(*args, **kwargs)
+        if 'date' in self.data:
+            try:
+                date = self.data.get('date')
+                available_slots = get_available_time_slots(date)
+                self.fields['time_slot'].choices = [(slot, slot) for slot in available_slots]
+            except (ValueError, TypeError):
+                self.fields['time_slot'].choices = []
+        else:
+            self.fields['time_slot'].choices = []
 
 
 
