42da42184ebf829fa444eda49d39d0b0bd992404
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-09 17:45:51 +0000

    Ensure template references correct URL

--- treatments/urls.py
+++ treatments/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.treatment_list, name='treatment_list'),
+    path('', views.treatment_list, name='treatments'), 
 ]
-
-
