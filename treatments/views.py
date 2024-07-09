13be8cc02fa04e73b64b6585d6357205bc7784c9
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-09 17:06:56 +0000

    Create a view to fetch the treatments

--- treatments/views.py
+++ treatments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Treatment
 
-def treatments_view(request):
-    return render(request, 'treatments/treatments.html')
-
+def treatment_list(request):
+    treatments = Treatment.objects.all()
+    return render(request, 'treatments/treatments.html', {'treatments': treatments})
