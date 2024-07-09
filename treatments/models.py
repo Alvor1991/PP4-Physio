1534e7190348fe9aa86fe2dce302c2b7c067a3f6
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-09 17:05:45 +0000

    Add treatments model

--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Treatment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='treatment_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
