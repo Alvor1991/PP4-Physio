d2c7a417e5116be9fd026a5f3ea18ae54f7f77e6
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-15 15:33:26 +0000

    Add comments to about models

--- about/models.py
+++ about/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class About(models.Model):
+    """
+    Model to store information about the website or business, 
+    including title, last updated date, and content.
+    """
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -10,6 +14,10 @@ class About(models.Model):
         return self.title
 
 class ContactRequest(models.Model):
+    """
+    Model to store contact requests submitted by users, 
+    including their name, email, message, read status, and the date created.
+    """
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
