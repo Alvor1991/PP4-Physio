4414f8e0cb4fd6f9418d5267a3a2dd68b6c8ebe1
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-15 19:52:30 +0000

    Add banner image to home and services offer section

--- home/views.py
+++ home/views.py
@@ -5,5 +5,6 @@ def home_view(request):
     View to render the home page.
     Simply returns the rendered home.html template.
     """
-    return render(request, 'home/home.html')
+    return render(request, 'home/home.html', {'banner_image': 'path/to/your/banner_image.jpg'})
+
 
