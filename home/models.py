216959425249b80e264fea7eb582de509e21e17a
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-16 19:26:48 +0000

    Create home model to allow admin edit

--- home/models.py
+++ home/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class HomePageContent(models.Model):
+    welcome_message = models.TextField()
+    banner_image = CloudinaryField('image', blank=True, null=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Home Page Content"
+
--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import HomePageContent
 
 def home_view(request):
     """
     View to render the home page.
     Simply returns the rendered home.html template.
     """
+    home_content = HomePageContent.objects.first()
     return render(request, 'home/home.html', {'banner_image': 'path/to/your/banner_image.jpg'})
 
 
