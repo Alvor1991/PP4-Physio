dd8ea31572f6dd6b682b9223b4492782eb9434ca
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-20 10:30:01 +0000

    Update appointment urls

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('book/', views.book_appointment, name='book_appointment'),
+    path('update/<int:pk>/', views.update_appointment, name='update_appointment'),
+    path('delete/<int:pk>/', views.delete_appointment, name='delete_appointment'),
     path('get_time_slots/', views.get_time_slots, name='get_time_slots'),
 ]
-
