b9009b06988ae959e54d41b60aeb0b15e22e7dca
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-20 10:08:40 +0000

    Set default value to physio

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,5 @@
+# Appointments/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -11,13 +13,15 @@ class Appointment(models.Model):
         ('physio', 'Physiotherapy'),
         ('massage', 'Massage Therapy'),
         ('acupuncture', 'Acupuncture'),
+        # Add more treatments as needed
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
     notes = models.TextField(blank=True, null=True)
-    treatment = models.CharField(max_length=50, choices=TREATMENT_CHOICES)
+    treatment = models.CharField(max_length=50, choices=TREATMENT_CHOICES, default='physio')
 
     def __str__(self):
         return f"Appointment for {self.user.username} on {self.date} at {self.time} for {self.treatment}"
+
