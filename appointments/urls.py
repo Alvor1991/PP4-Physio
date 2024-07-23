3b3c17791f5030d7427fe79e91c31aca54687d9f
Ryan Nolan <ryannolan_@hotmail.com>
2024-07-23 18:09:58 +0000

    Appointment date and time issue fixed

--- appointments/urls.py
+++ appointments/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('update/<int:pk>/', views.update_appointment, name='update_appointment'),
     path('delete/<int:pk>/', views.delete_appointment, name='delete_appointment'),
     path('get_time_slots/', views.get_time_slots, name='get_time_slots'),
-    path('my_appointments/', views.user_appointments, name='user_appointments'),  # New URL pattern
+    path('my_appointments/', views.user_appointments, name='user_appointments'), 
 ]
