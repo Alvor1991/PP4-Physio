070c48f84ad83bd58ead644369ea50913a6028b1
Ryan Nolan <ryannolan_@hotmail.com>
2024-08-02 20:16:11 +0000

    Add welcome message for new account users

--- home/adapter.py
+++ home/adapter.py
@@ -1,7 +1,13 @@
 from allauth.account.adapter import DefaultAccountAdapter
-from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 class CustomAccountAdapter(DefaultAccountAdapter):
+    def get_signup_redirect_url(self, request):
+        """
+        Return the URL to redirect to after a successful signup.
+        """
+        return reverse('welcome')  # Use the name of your welcome page URL pattern
+
     def add_message(self, request, level, message_template, message_context=None, extra_tags=''):
         # Suppress the "Confirmation email sent" message
         if message_template == 'account/messages/email_confirmation_sent.txt':
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('welcome/', TemplateView.as_view(template_name='welcome.html'), name='welcome'),
 ]
-
-
