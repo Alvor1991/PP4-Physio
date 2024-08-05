70e7f64f69d2e2688f4735e057df2e1a2a92c20c
Ryan Nolan <ryannolan_@hotmail.com>
2024-08-05 12:31:11 +0000

    Utilise bootstrap for delete appointment modal

--- treatments/views.py
+++ treatments/views.py
@@ -4,4 +4,4 @@ from .models import Treatment, FAQ
 def treatment_list(request):
     treatments = Treatment.objects.all()
     faqs = FAQ.objects.all()
-    return render(request, 'treatments/treatments.html', {'treatments': treatments, 'faqs': faqs})
+    return render(request, 'treatments/treatments.html', {'treatments': treatments, 'FAQs': FAQs})
