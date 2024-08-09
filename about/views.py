aecae27b937462a70721569899b70e2ef897b6ec
Ryan Nolan <ryannolan_@hotmail.com>
2024-08-09 21:50:32 +0000

    Update color shcheme, typography and about page responsiveness

--- about/views.py
+++ about/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib import messages
 from django.http import JsonResponse
 from .models import About, ContactRequest
 from .forms import ContactForm
@@ -14,7 +13,6 @@ def about_me(request):
         contact_form = ContactForm(data=request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.add_message(request, messages.SUCCESS, "Your message has been sent successfully. I endeavor to respond within 2 working days.")
             if request.headers.get('x-requested-with') == 'XMLHttpRequest':
                 return JsonResponse({'success': True})
 
